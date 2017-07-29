defb4ee222f4508741fbf6eb3b908331569056c0
Farid Ariafard <faridsenior@gmail.com>
2017-07-29 14:07:01 -0400

    Update Solution class

--- Solution.java
+++ Solution.java
@@ -3,13 +3,10 @@
  * Reverse digits of an integer.
  * Example1: x = 123, return 321
  * Example2: x = -123, return -321
- * The input is assumed to be a 32-bit signed integer. Your function should return 0 when the reversed integer overflows
- * 1000000003 This is a example input for the overflow
- **********************************
- * Determine whether an integer is a palindrome. Do this without extra space.
+ * The input is assumed to be a 32-bit signed integer. Your function should return 0 when the reversed integer overflows.
+ * 1000000003 This is a example input for the overflow.
  */
 public class Solution {
-
 
     public int reverse(int x) {
         String intString = Integer.toString(x);
@@ -25,39 +22,17 @@ public class Solution {
         else {
             resultString = SB.toString();
         }
-
         Long result = Long.parseLong(resultString);
         if (result>Integer.MAX_VALUE ||result<Integer.MIN_VALUE)
             result = Long.valueOf(0);
         return Math.toIntExact(result);
     }
 
-    public boolean isPalindrome(int x) {
-        if (x<0)return false;
-        String string = Integer.toString(x);
-        int pointerForward = 0;
-        int pointerBackward = string.length()-1;
-        boolean identical = false;
-
-        while (pointerForward<=pointerBackward){
-            if(string.charAt(pointerForward) == string.charAt(pointerBackward)){
-                identical = true;
-            }
-            else {
-                return false;
-            }
-            pointerForward++;
-            pointerBackward--;
-        }
-        return identical;
-
-    }
-
     public static void main(String[] args) {
+        
         Solution SL = new Solution();
 
         System.out.println(SL.reverse(-321));
-        System.out.println(SL.isPalindrome(0));
+        System.out.println(SL.reverse(5678));
     }
-
 }
